80597294de49ce76ce4b29329c11074994c9e2a7
egor <eegor650@gmail.com>
2018-03-13 13:08:31 -0700

    added normalize.css

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 var Main = require('./components/Main')
+import 'normalize.css/normalize.css'
 import './styles/styles.css'
 
 ReactDOM.render(
